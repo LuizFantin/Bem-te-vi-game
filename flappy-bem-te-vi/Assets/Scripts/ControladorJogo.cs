1179a003a4408227227a24e41c519ff6db6ef1e8
Luiz Fantin <fantin_luiz@outlook.com>
2021-09-16 17:27:10 -0300

    feat: sound and improve dificult

--- flappy-bem-te-vi/Assets/Scripts/ControladorJogo.cs
+++ flappy-bem-te-vi/Assets/Scripts/ControladorJogo.cs
@@ -8,10 +8,14 @@ public class ControladorJogo : MonoBehaviour
 {
     public int pontuacao;
     public GameObject fundoNoite;
+    public GeraCanos geraCanos;
     public Text textoPontuacao;
     public Text textoRecorde;
-    public int contadorNoite = 0;
     public int limiteNoite;
+    public int contadorNoite = 0;
+
+
+    public AudioSource grito;
 
     private bool noite = false;
     
@@ -22,6 +26,7 @@ public class ControladorJogo : MonoBehaviour
     void Start() {
         Time.timeScale = 1; // despausa o jogo
         textoRecorde.text = "Recorde: " + PlayerPrefs.GetInt("Recorde");
+        geraCanos = FindObjectOfType<GeraCanos>();
     }
 
     void Update() {
@@ -29,11 +34,32 @@ public class ControladorJogo : MonoBehaviour
             noite = !noite;
             contadorNoite = 0;
             fundoNoite.SetActive(noite);
+            grito.Play();
         }
 
         if(pontuacao > PlayerPrefs.GetInt("Recorde")){
             PlayerPrefs.SetInt("Recorde", pontuacao);
         }
         textoRecorde.text = "Recorde: " + PlayerPrefs.GetInt("Recorde");
+
+        if(pontuacao >= 1 && pontuacao <= 9){
+            geraCanos.intervaloGeracao = 3.5f;
+        }
+
+        if(pontuacao > 9 && pontuacao <= 19){
+            geraCanos.intervaloGeracao = 3f;
+        }
+
+        if(pontuacao > 19 && pontuacao <= 29){
+            geraCanos.intervaloGeracao = 2f;
+        }
+
+        if(pontuacao > 29 && pontuacao <= 39){
+            geraCanos.intervaloGeracao = 1.5f;
+        }
+
+        if(pontuacao > 39){
+            geraCanos.intervaloGeracao = 1.2f;
+        }
     }
 }
