7e96fd77aded171e6699d96550ae605d86323604
Milena Tavares <milenaroas48@gmail.com>
2021-10-01 21:59:49 -0300

    Menu Funcionando

--- flappy-bem-te-vi/Assets/Scripts/Menu.cs
+++ flappy-bem-te-vi/Assets/Scripts/Menu.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Função utilizada para carregar determinada cena
+    public void LoadScene(int indexScene) //O parâmetro define a cena que deve ser carregada
     {
-        
+        SceneManager.LoadScene(indexScene);
     }
 
-    // Update is called once per frame
-    void Update()
+    //Função utilizada para encerrar o jogo
+    public void ExitScene()
     {
-        
+        Application.Quit();
     }
+
+
 }
